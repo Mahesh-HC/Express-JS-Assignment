5c6b5f97b7eeb9106ece29f3141e8f956b895bea
Mahesh H C <109088120+Mahesh-HC@users.noreply.github.com>
2022-11-21 06:37:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fs=require('fs')
+const bp=require('body-parser')
+
+const app = express();
+app.use(bp.json())
+const port = process.env.PORT || 5000;
+
+app.post('/student/add', (req,res)=>{
+  fs.writeFileSync("./student.json", (JSON.stringify(req.body)))
+  res.send(JSON.stringify({"result": "success"}))
+})
+
+app.get('/student/getDetails', (req,res)=>{
+    const details=fs.readFileSync("./student.json", "utf-8")
+    res.send(details)
+})
+
+app.get("/studentList", (req,res)=>[
+    res.status(200).send("notes from api")
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
