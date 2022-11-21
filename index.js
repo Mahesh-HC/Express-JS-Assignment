067cb68e6639ae3f80b5793a16e537335145006f
Mahesh H C <109088120+Mahesh-HC@users.noreply.github.com>
2022-11-21 12:39:02 +0000

    final commit

--- index.js
+++ index.js
@@ -6,29 +6,32 @@ app.use(bp.json())
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Welcome it's working")
+  res.status(200).send("Welcome it's working")
 })
 
 app.post('/student/add', (req, res) => {
-    fs.writeFileSync("./Data/student.json", (JSON.stringify(req.body)))
-    res.status(200).send(JSON.stringify({ "result": "success" }))
+  fs.writeFileSync("./Data/student.json", (JSON.stringify(req.body)))
+  res.status(200).send(JSON.stringify({ "result": "success" }))
 })
 
 app.get('/student/getDetails', (req, res) => {
-    const details = fs.readFileSync("./Data/student.json", "utf-8")
-    res.status(200).send(details)
+  const details = fs.readFileSync("./Data/student.json", "utf-8")
+  res.status(200).send(details)
 })
 
 app.get("/student/studentLists", (req, res) => {
-    const data = fs.readFileSync("./Data/users.json", "utf-8")
-    res.status(200).send(data)
+  const data = fs.readFileSync("./Data/users.json", "utf-8")
+  res.status(200).send(data)
 
 })
 app.all('*', (req, res) => {
-    res.status(404)
-    res.send("page not found")
+  res.status(404)
+  res.send("page not found")
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 });
+
+
+/* Heroku URL :  https://murmuring-crag-12803.herokuapp.com/ */
--- problem2.js
+++ problem2.js
@@ -11,8 +11,28 @@ app.get("/student/studentLists", (req, res) => {
 
 app.all('*', (req, res) => {
   res.status(404)
+  res.send("page not found")
 })
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 });
+
+/*
+For Heroku => Link: https://murmuring-crag-12803.herokuapp.com/
+in package.json add 
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node index.js"
+  }
+
+  steps for heroku :
+  1. heroku login => opens login page(login)
+  2. git init
+  3. git add . 
+  4. git commit -m "comment"
+  5. heroku create (app name is optional u can provide or automatically assigned)
+  6. git push heroku master 
+  to change the repository go to the heroku website, select the repository to be added and click and go to deploy and follow the steps provided.
+  ex => heroku git:remote -a murmuring-crag-12803, and then continue.
+*/
